ecb8c81d92a2b1257e1139596be5e60de0bcec0c
0xSchindler <leduchien8901@gmail.com>
2024-07-26 23:37:23 +0700

    fix margin

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -14,19 +14,8 @@ export default function Layout({ children }: { children: ReactNode }) {
                 display: "flex",
             }}
         >
-            <Box
-                sx={{
-                    width: sidebarWidth,
-                    backgroundColor: "#f4f4f4", // Example background color
-                    height: "100vh",
-                    position: "fixed", // Fixed position for sidebar
-                    left: 0,
-                    boxShadow: "0px 0 px rgba(0,0,0,0.1)", // Add shadow for aesthetics
-                    zIndex: 1200, // Ensure it's on top of content
-                }}
-            >
-                <Sidebar />
-            </Box>
+
+            <Sidebar />
 
             <Box
                 component="main"
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
 import RouterUrl from "./routes.tsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
